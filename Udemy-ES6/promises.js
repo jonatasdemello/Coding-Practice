d6ef915634f6b6874e5e3989a00cad5a7de2914d
jonatas <jonatas.mello@hotmail.com>
2018-12-20 09:22:03 -0500

    organize files

--- Udemy-ES6/promises.js
+++ Udemy-ES6/promises.js
@@ -51,3 +51,16 @@ ps4 = new Promise((resolve, reject) => {
     };
 });
 
+
+console.log('------------- promises-fetch');
+
+url = "https://jsonplaceholder.typicode.com/posts/";
+
+//fetch(url).then(data => console.log(data));
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log('Error: ', error));
+
+// does not catch 300 errors
--- Udemy-ES6/validate.js
+++ Udemy-ES6/validate.js
@@ -1,21 +1,18 @@
-function Fn_validate(){
-
-    function Field(val){
-        this.value = val;
-    }
-    
-    Field.prototype.validate = function(){
-        return this.value.length > 0;
-    }
-    
-    var username = new Field("myuser");
-    var password = new Field("pass");
-    
-    var fields = [username, password];
-    
-    var formIsValid = fields.every(function(f){
-        return f.validate();
-    });
-    
-    console.log(formIsValid);
+function Field(val) {
+  this.value = val;
 }
+
+Field.prototype.validate = function () {
+  return this.value.length > 0;
+}
+
+var username = new Field("myuser");
+var password = new Field("pass");
+
+var fields = [username, password];
+
+var formIsValid = fields.every(function (f) {
+  return f.validate();
+});
+
+console.log(formIsValid);
