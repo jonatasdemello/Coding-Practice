f0db4d0339730680c1f13637153628d66f6606f1
jonatas <jonatas.mello@hotmail.com>
2019-01-14 11:05:05 -0500

    refactoring

--- Udemy-ES6/classes.js
+++ Udemy-ES6/classes.js
@@ -1,3 +1,4 @@
+console.log('------------- getter');
 // using getter
 class Rectangle {
     constructor(height, width) {
@@ -16,6 +17,7 @@ class Rectangle {
 const square = new Rectangle(10, 10);
 console.log(square.area); // 100
 
+console.log('------------- static-methods');
 // static methods:
 class Point {
     constructor(x, y) {
@@ -36,9 +38,32 @@ const p2 = new Point(10, 10);
 // static method is called in the class, not the object:
 console.log(Point.distance(p1, p2)); // 7.0710678118654755
 
+console.log('------------- prototype');
+// using prototype:
+function CarP(options) {
+  this.title = options.title;
+}
+CarP.prototype.drive = function() {
+  return 'vroom';
+}
+const carProto = new CarP({title: 'Focus'});
+console.log("carProto.drive(): ", carProto.drive());
+console.log("carProto: ", carProto);
 
+// or 
+CarP.prototype ={
+  start: function() {
+    return 'on';
+  },
+  stop: function() {
+    return 'off';
+  }
+}
+
+
+console.log('------------- samples');
 class Car {
-    constructor({title}) {
+    constructor( {title} ) { // destructuring object => options.title
         this.title = title;
     }
     drive() {
@@ -47,20 +72,21 @@ class Car {
 }
 
 class Toyota extends Car {
-    constructor({color}) {
-        super({
-            title: "toyota"
-        });
+    constructor( {color} ) {
+        super( { title: "toyota" } ); // calls base class' constructor
         this.color = color;
     }
     honk() {
         return 'beep';
     }
 }
+// create a new object
+const ford = new Car( {title: "Focus"} );
+console.log("ford car: ", ford);
+console.log("ford car drive: ", ford.drive());
 
-const toyota = new Toyota({
-    color: 'red'
-});
+const toyota = new Toyota( { color: 'red' } ); // options object
+
 console.log("car: ", toyota);
 console.log("calling method honk:", toyota.honk());
 console.log("calling method drive:", toyota.drive());
--- Udemy-ES6/destructuring.js
+++ Udemy-ES6/destructuring.js
@@ -2,7 +2,7 @@ console.log('------------- destructuring');
 
 var expense = {
   type: 'Business',
-  amount: '$45'
+  amount: '$45 USD'
 };
 // const type = expense.type;
 // const amount = expense.amount;
@@ -12,8 +12,10 @@ var expense = {
 
 { // normal
   console.info("------------- normal:");
+  
   let type = expense.type;
   let amount = expense.amount;
+
   console.log("fist:", type, amount);
 
   function show(expense) {
@@ -25,26 +27,31 @@ var expense = {
 // or
 {
   console.info("------------- destructuring:");
+  
   function show({ type }) {
     console.log("type: ", type);
   }
   show(expense);
   // var name has to match ther property name!
+  
   let { type, amount } = expense;
+  
   console.log("second: ", type, amount);
+  
   let { myType } = expense;
+  
   console.log("undefined: ", myType); // undefined because myType does not exist
 }
 
-console.log('------------- destructuring');
-
+console.log('------------- arguments-object');
+// get by property name
 var savedFile = {
   ext: 'jpg',
   name: 'repost',
   size: 14040
 };
 
-function fileSummary(file) {
+function fileSummary0(file) {
   return `Summary 0: the file ${file.name}.${file.ext} is of size ${file.size}`;
 }
 //ES6
@@ -52,145 +59,136 @@ function fileSummary1({ name, ext, size }) {
   return `Summary 1: the file ${name}.${ext} is of size ${size}`;
 }
 
-function fileSummary2({
-  name,
-  ext,
-  size
-}, {
-  color
-}) {
+function fileSummary2({ name, ext, size }, { color }) {
   return `Summary 2: ${color} the file ${name}.${ext} is of size ${size}`;
 }
-console.log(fileSummary(savedFile));
+console.log(fileSummary0(savedFile));
 console.log(fileSummary1(savedFile));
-console.log(fileSummary2(savedFile, {
-  color: 'Red'
-}));
+console.log(fileSummary2(savedFile, { color: 'Red'}));
 
 
-function Fn_Destructuring_2() {
-  const companies = ['Google', 'Facebook', 'Uber'];
+console.log('------------- arrays');
+// get items from the array
+const companies = ['Google', 'Facebook', 'Uber'];
 
-  let [name] = companies;
-  console.log(name);
+let [name] = companies;
+console.log(name); // same as companies[0] = Google
 
-  let [name1, name2, name3] = companies;
-  console.log(name1, name2, name3);
+// use [] to get items and {} to get properties
+let { length } = companies; // name is not a property
+console.log(length); // = 3
 
-  let [nameA, nameB, nameC, nameD] = companies;
-  console.log(nameA, nameB, nameC, nameD);
+let [name1, name2, name3] = companies;
+console.log(name1, name2, name3);
 
-  let [first, ...rest] = companies;
-  console.log(first, "and,", rest);
-}
+let [nameA, nameB, nameC, nameD] = companies;
+console.log(nameA, nameB, nameC, nameD); // nameD is undefined
 
-function Fn_Destructuring_3() {
-  const companies = [{
-      name: 'Google',
-      location: "Montain View"
-    },
-    {
-      name: 'Facebook',
-      location: "Menlo Park"
-    },
-    {
-      name: 'Uber',
-      location: "San Francisco"
-    }
-  ];
-  console.log(companies[1].location);
-
-  let [obj] = companies;
-  console.log(obj); // {location: "Montain View", name: "Google"}
-
-  let [{
-    location
-  }] = companies;
-  console.log(location); // Montain View only
-
-  const Google = {
-    locations: ["Montain View", "New York", "London"]
-  };
-  //console.log(Google);
-
-  let {
-    locations: lc
-  } = Google;
-  console.log("lc", lc); // ["Montain View", "New York", "London"]
-
-  let {
-    locations: [loc]
-  } = Google;
-  console.log("loc:", loc); //Montain View
+let [first, ...rest] = companies;
+console.log("first: ", first, "and rest:", rest);
 
 
-}
+console.log('------------- arrays-objects');
 
-function Fn_Destructuring_4() {
-  function signup({
-    username,
-    password
-  }) {
-    console.log(username, password)
+const companiesObj = [{
+    name: 'Google',
+    location: "Montain View"
+  },
+  {
+    name: 'Facebook',
+    location: "Menlo Park"
+  },
+  {
+    name: 'Uber',
+    location: "San Francisco"
   }
-
-  function signup1({
-    username,
-    password,
-    email
-  }) {
-    console.log(username, password, email)
-  }
-  const user = {
-    username: "myname",
-    password: "pass",
-    email: "my@gmail.com"
-  }
+];
+console.log(companiesObj[1].location);
+{
+  let [location] = companiesObj;
+  console.log("location1: ", location); // {name: "Google", location: "Montain View"}
+}
+// or destructuring both arrays-objects
+{
+  let [{  location }] = companiesObj;
+  console.log("location1: ", location); // Montain View only
 }
 
-function Fn_Destructuring_5() {
-  const points = [
-    [4, 5],
-    [1, 2],
-    [3, 7]
-  ];
-  let mypoints = [{
-      x: 1,
-      y: 2
-    },
-    {
-      x: 0,
-      y: 0
-    }
-  ];
-  let pt1 = points.map(pair => {
-    //console.log(pair[0], pair[1]); 
-    const [x, y] = pair;
-    return {
-      x: x,
-      y: y
-    };
-  });
-  console.log("pt1", pt1);
+const Google = {
+  locations: ["Montain View", "New York", "London"]
+};
+console.log("Google obj: ", Google);
 
-  let pt2 = points.map(([x, y]) => {
-    return {
-      x: x,
-      y: y
-    };
-  });
-  console.log("pt2", pt2);
+let { locations } = Google;
+console.log("property locations: ", locations); // ["Montain View", "New York", "London"]
 
-  let pt3 = points.map(([x, y]) => {
-    return {
-      x,
-      y
-    }; //object literal sintax
-  });
-  console.log("pt3", pt3);
+let { locations: locationArray } = Google;
+console.log("locations Array: ", locationArray); // ["Montain View", "New York", "London"]
+
+let { locations: [location] } = Google;
+console.log("first element location: ", location); // Montain View
+
+
+
+
+console.log('------------- when-to-use');
+
+function signup0(username, password) {
+  console.log("signup0: ", username, password); // simple variable
+}
+signup0('myname0', 'mypassword0');
+
+function signup1({ username, password }) {
+  console.log("signup1: ", username, password);
 }
 
-function Fn_Destructuring_6() {
+function signup2({ username, password, email }) {
+  console.log("signup2: ", username, password, email);
+}
+const user = {
+  username: "myname",
+  password: "pass",
+  email: "my@gmail.com"
+}
+signup1(user);
+signup2(user);
+
+
+console.log('------------- array-destructuring');
+
+// have an array of arrays
+const points = [
+  [4, 5],
+  [1, 2],
+  [3, 7]
+];
+// but we need an array of objects
+let mypoints = [
+  { x: 1, y: 2 },
+  { x: 0, y: 0 }
+];
+
+let pt1 = points.map(pair => {
+  //console.log("x:", pair[0], "y:", pair[1]); 
+  const [x, y] = pair;
+  return { x: x, y: y };
+});
+console.log("pt1", pt1);
+
+// or
+let pt2 = points.map(([x, y]) => { // destructuring in the argument list
+  return { x: x, y: y };
+});
+console.log("pt2", pt2);
+
+let pt3 = points.map(([x, y]) => {
+  return { x, y }; // simplify with object literal sintax
+});
+console.log("pt3", pt3);
+
+
+console.log('------------- samples');
+
   const profile = {
     title: 'Engineer',
     department: 'Engineering'
@@ -203,10 +201,7 @@ function Fn_Destructuring_6() {
   }
 
   function isEngineer(profile) {
-    let {
-      title,
-      department
-    } = profile;
+    let { title, department } = profile;
     return title === 'Engineer' && department === 'Engineering';
   }
 
@@ -223,23 +218,15 @@ function Fn_Destructuring_6() {
   //console.log(subject, time, teacher);
 
   let classesAsObject = classes.map(([subject, time, teacher]) => {
-    return {
-      subject,
-      time,
-      teacher
-    };
+    return { subject, time, teacher };
   });
   console.log(classesAsObject);
 
-}
 
-function Fn_Destructuring_7() {
-  const numbers = [1, 2, 3];
 
-  function double(arr) {
-    return arr.map(num => {
-      return num * 2;
-    });
-  }
-  console.log(double([1, 2, 3]));
+const numbers = [1, 2, 3];
+
+function double(arr) {
+  return arr.map(num => num * 2);
 }
+console.log(numbers, double([1, 2, 3]));
