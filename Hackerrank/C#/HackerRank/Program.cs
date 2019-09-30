cbdede9f6dbb192dc2a70f2bd8ffe70b00bceeab
jonatas <jonatas.mello@hotmail.com>
2019-09-29 21:16:23 -0400

    C# HackerRank

---
+++ Hackerrank/C#/HackerRank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Solution
+    {
+        public static void Main(String[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            Console.WriteLine(typeof(ChocolateFeast).Name);
+            ChocolateFeast.entry(args);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Hackerrank/C#/README.md
@@ -0,0 +1,17 @@
+HackerRank
+==========
+
+Solutions to the Challenges and Contests in HackerRank.  
+You are welcome to let me know if you can optimize my code even better.
+
+####Challenges Completed:  
+- Gem Stone  
+- Utopian Tree  
+- Service Lane  
+- The Love-Letter Mystery  
+- Halloween Party  
+- Cut the Sticks  
+- Maximizing XOR  
+- Game of Thrones - I  
+- Chocolate Feast  
+
