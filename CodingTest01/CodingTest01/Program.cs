9b4787cbcb4dd639a5a820dc2704579ae0b7195d
jonatas <jonatas.mello@hotmail.com>
2019-10-13 10:57:04 -0400

    new search test

--- CodingTest01/CodingTest01/Program.cs
+++ CodingTest01/CodingTest01/Program.cs
@@ -15,6 +15,8 @@ namespace CodingTest01
             //var res = CyclicRotation.Solution(new int[] { 1, 2, 3, 4, 5 }, 2);
             //Console.WriteLine("result: {0}", string.Join(",", res));
 
+            CompareSearch.CompareSearches();
+
             Console.WriteLine("nothing to show...");
             //Console.ReadKey();
         }
